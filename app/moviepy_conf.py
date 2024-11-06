4a0d907e68b6c85b2ecad2df317035c88c5d2af8
Romeu Beato <romeufbeato@gmail.com>
2024-11-06 13:44:42 +0000

    Added MoviePy configuration for proper ImageMagick path handling

--- app/moviepy_conf.py
+++ app/moviepy_conf.py
@@ -1,8 +1,5 @@
 from moviepy.config import change_settings
-import os
+from .config import Config
 
-# Set the path to ImageMagick binary - Updated with correct folder name
-IMAGEMAGICK_BINARY = r"C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\magick.exe"
-
-# Configure MoviePy to use ImageMagick
-change_settings({"IMAGEMAGICK_BINARY": IMAGEMAGICK_BINARY})
+# Set MoviePy's ImageMagick binary path
+change_settings({"IMAGEMAGICK_BINARY": Config.IMAGEMAGICK_BINARY})
