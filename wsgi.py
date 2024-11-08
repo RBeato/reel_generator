25a2107f62bc8f74744b7d495f25e9eae5959c79
Romeu Beato <romeufbeato@gmail.com>
2024-11-08 15:13:18 +0000

    Add WSGI and Nginx configuration files

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,6 @@
 from app import create_app
 
 app = create_app()
-# app.config['TIMEOUT'] = 300
-# app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
-# app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-# app.config['REQUEST_TIMEOUT'] = 300 
+
+if __name__ == '__main__':
+    app.run()
